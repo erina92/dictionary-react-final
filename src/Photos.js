f122cbbaae569f6f58cb9d23ff8ebe9ebb550ac7
erina92 <erikamiglietta92@gmail.com>
2023-05-24 12:27:42 +0200

    added link to show original pic

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,9 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-lg-4 col-md-6 col-sm-12" key={index}>
-                <img src={photo.src.tiny} alt={photo.alt} key={index} />
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.tiny} alt={photo.alt} key={index} />
+                </a>
               </div>
             );
           })}
